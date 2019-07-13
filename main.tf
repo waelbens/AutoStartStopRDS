c9e200e2eec4dcfe01565aaa6fe08c8cf925ac82
waelbens <wael.ben.salah@gmail.com>
2019-07-13 02:38:44 +0200

    Fix zipfile path

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "rds-lambda-stop-start" {
   handler          = "index.handler"
   timeout          = "5"
   runtime          = "nodejs6.10"
-  source_code_hash = "${base64sha256(file("../functions/rds-lambda-stop-start.zip"))}"
+  source_code_hash = "${base64sha256(file("functions/rds-lambda-stop-start.zip"))}"
 }
 
 resource "aws_iam_role" "LambdaRDSManagement" {
